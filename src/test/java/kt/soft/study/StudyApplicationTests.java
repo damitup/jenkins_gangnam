e97099b1d8548278be1e244a262c5ead80a24cca
NHD <damitup.00@gmail.com>
2025-08-01 10:41:32 +0900

    first

---
+++ src/test/java/kt/soft/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kt.soft.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
